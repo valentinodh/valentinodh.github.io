2b928848901c43c0f8a77971d72582ceeae1a6ca
val <valntino@myyahoo.com>
2025-01-10 22:06:25 +0100

    Add files via upload

--- files/anti_skid.js
+++ files/anti_skid.js
@@ -1,10 +1,5 @@
-window.addEventListener("keydown", (e) => {
-    const forbiddenKeys = ["s", "c", "e", "i", "k", "u"];
-    if (e.ctrlKey && forbiddenKeys.includes(e.key.toLowerCase())) {
-        e.preventDefault();
-    }
-});
-
-document.addEventListener("contextmenu", (e) => {
-    e.preventDefault();
+window.addEventListener("keydown", e => {
+    e.ctrlKey && ("S" === e.key || "s" === e.key) && e.preventDefault(), e.ctrlKey && ("C" === e.key || "c" === e.key) && e.preventDefault(), e.ctrlKey && ("E" === e.key || "e" === e.key) && e.preventDefault(), e.ctrlKey && ("I" === e.key || "i" === e.key || "\xc3„\xc2\xb1" === e.key) && e.preventDefault(), e.ctrlKey && ("K" === e.key || "k" === e.key) && e.preventDefault(), e.ctrlKey && ("U" === e.key || "u" === e.key) && e.preventDefault()
+}), document.addEventListener("contextmenu", function(e) {
+    e.preventDefault()
 });
